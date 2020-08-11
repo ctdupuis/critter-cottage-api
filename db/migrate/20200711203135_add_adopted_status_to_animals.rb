796245931126c181d199d5edde3992c7b929a7fc
ctdupuis <ctdupuis7@gmail.com>
2020-08-11 17:46:04 -0500

    first production build

--- db/migrate/20200711203135_add_adopted_status_to_animals.rb
+++ db/migrate/20200711203135_add_adopted_status_to_animals.rb
@@ -1,6 +1,5 @@
 class AddAdoptedStatusToAnimals < ActiveRecord::Migration[6.0]
   def change
     add_column :animals, :adopted, :boolean
-    # ALTER TABLE animals ALTER COLUMN adopted SET DEFAULT false
   end
 end
